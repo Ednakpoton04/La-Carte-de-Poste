8ac37547457ac0f9548415f5bc668e98bdf33677
Ednakpoton04 <ednakpoton630@gmail.com>
2025-02-19 15:08:54 +0100

    la carte de poste

---
+++ jessica.js
@@ -0,0 +1,16 @@
+// alert ("edna");
+// let prenom;
+// prenom=Edna;
+// console.log(prenom);
+// console.log();
+
+let totalLivres;
+totalLivres=500;
+totalLivres +=50;
+totalLivres -=10;
+totalLivres +=5;
+
+console.log(totalLivres);
+
+total= "Notre bibliothèque possède "  + totalLivres + " livres";
+console.log(total);
